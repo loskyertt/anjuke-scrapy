bddb013a0da0d82f0a1b09142cf9c33938f89d73
loskyertt <loskyertt0403@gmail.com>
2024-10-17 21:14:47 +0800

    删除proxy_pool

--- test/testProxy.py
+++ test/testProxy.py
@@ -9,11 +9,9 @@ def delete_proxy(proxy):
 # your spider code
 
 def getHtml(url, headers):
-    # ....
     retry_count = 15
     while retry_count > 0:
         proxy = get_proxy().get("proxy")
-        # proxy = {"http": "http://{}".format(proxy)}
         proxies = {
             "http": "http://" + proxy,
             "https": "http://" + proxy
@@ -29,10 +27,9 @@ def getHtml(url, headers):
             delete_proxy(proxy)
     return None
 
-# url = "https://wuhan.anjuke.com/sale/p1/?from=esf_list"
-url = "https://wuhan.anjuke.com/sale/"
+url = "https://wuhan.anjuke.com/sale/p1/?from=esf_list"
 headers = {
-    "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
     "Cookie":  "aQQ_ajkguid=3CA5E24D-247C-44FD-9E3F-6D28E21E569D; sessid=5A00BB77-8287-42CA-9800-F5952974A9F5; ajk-appVersion=; id58=Cr4A2Wbta7Q52sUIHAP+Ag==; xxzlclientid=eee16be4-5229-4972-a6ff-1726835637144; xxzlxxid=pfmxYBIGZjqG7smlG9onwVYXB6SLni8I9YjtZKpTCcvCO/z69FfwRyLwwMFlpIlfP10x; ctid=22; fzq_h=26d3b7c046e744f75ba5ae2f716cc081_1729042014853_998bb6cd38b2472cb94f440446765720_1746730543; fzq_js_anjuke_ershoufang_pc=e279987116902c61872e31e0dec7697d_1729080567676_24; twe=2; obtain_by=2; xxzlbbid=pfmbM3wxMDM0NnwxLjEwLjB8MTcyOTA4MDU3MTYzMTA1NDU3OHx0K2lIOXBGU3lHM2Z6ajN6enJHYXVibWt6SWxXSDVieEtkNmtaZkNjckxJPXwyOTY5MDQ2NjJmNzdjOGVlNGEwNDc3NGYzYTJlZmVjOV8xNzI5MDgwNTcwMjgyXzkzN2Y0ZGNiMGNlNzQ0ODRiNjY3M2Q1N2FlNGYxZWM0XzE3NDY3MTU5MTh8YWU2MTg0OTkyYWMyZmY4MGI3Mzg4MGUwMjdlN2NiMTBfMTcyOTA4MDU3MDYzN18yNTY="
 }
 
